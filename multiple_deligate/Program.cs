a8ba0b291940585b56232304e63e5b4e073d0e4f
nawsish <nawsish2018@gmail.com>
2025-03-04 15:23:04 +0600

    add deligate 2

---
+++ multiple_deligate/Program.cs
@@ -0,0 +1,12 @@
+﻿public delegate void MyDelegate(string message);
+class Program
+{
+    public static void ShowMessage(string msg) => Console.WriteLine("Message: " + msg);
+    public static void AnotherMethod(string msg) => Console.WriteLine("Another: " + msg);
+    static void Main()
+    {
+        MyDelegate del = ShowMessage;
+        del += AnotherMethod;
+        del("Hello!");
+    }
+}
