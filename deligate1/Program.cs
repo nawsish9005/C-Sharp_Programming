eb414768883044125e7a4faa707cae2dc22dc35f
nawsish <nawsish2018@gmail.com>
2025-03-04 15:21:20 +0600

    add deligate

---
+++ deligate1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public delegate void MyDelegate(string message);
+class Program
+{
+    public static void ShowMessage(string msg)
+    {
+        Console.WriteLine("Message: " + msg);
+    }
+    static void Main()
+    {
+        MyDelegate del = new MyDelegate(ShowMessage);
+        del("Hello, Delegates!");
+    }
+}
